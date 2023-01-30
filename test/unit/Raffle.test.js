2666608f98bf271224638515bece1c2d2abaefe2
Teerth Patel <76660982+teerth17@users.noreply.github.com>
2023-01-30 17:25:13 +0530

    Update Raffle.test.js

--- test/unit/Raffle.test.js
+++ test/unit/Raffle.test.js
@@ -181,7 +181,6 @@ const { developmentChains, networkConfig } = require("../../helper-hardhat-confi
                           txReceipt.events[1].args.requestId,
                           raffle.address
                       )
-                      console.log("Finish")
                   })
               })
           })
